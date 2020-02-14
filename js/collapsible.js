5f8432790bd1ba991e84b27c5926daf6cc903261
X-Ryl669 <boite.pour.spam@gmail.com>
2020-02-14 11:47:36 -0500

    Allow folding an unfolded section. This only works if javascript is running.

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event){ 
-  document.querySelectorAll("label.collapsible").forEach(function(el) { el.addEventListener("click", function(ev) { setTimeout(function(){ev.target.scrollIntoView({behavior: "smooth"})}, 100) }) })
-})
+document.addEventListener("DOMContentLoaded", function(event){
+  document.querySelectorAll("label.collapsible").forEach(function(el) {
+    el.addEventListener("click", function(ev) { setTimeout(function(){ev.target.scrollIntoView({behavior: "smooth"})}, 100)});
+    document.getElementById(el.getAttribute("for")).addEventListener("click", function(ev) {
+      if (ev.target.previous) ev.target.checked = false;
+      if (ev.target.checked) document.querySelectorAll("input.collapsible").forEach(function(i) { i.previous = false });
+      ev.target.previous = ev.target.checked;
+    });
+})});
